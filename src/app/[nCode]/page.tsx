4b5eeaeafa3d4f78e5b1686da55cf38c9cd51d36
7savv7 <savchrisostomidhs@hotmail.com>
2025-03-25 20:09:46 +0200

    initial commit

---
+++ src/app/[nCode]/page.tsx
@@ -0,0 +1,80 @@
+import Image from "next/image";
+import Link from "next/link";
+import data from "../../../data.json";
+import "./country.css"
+
+async function Country({ params }: { params: Promise<{ nCode: string }> }) {
+    const code = (await params).nCode;
+    let country = data.find(d => d.numericCode === code);
+
+    // Function used to return formatted string with country's currencies or languages
+    function format(array: { name: string }[] | undefined): string {
+        if (array === undefined) return "undefined";
+        let languages: string = "";
+        array.forEach((el, index) => {
+            languages += el.name;
+            if (index < array.length - 1) {
+                languages += ", ";
+            }
+        });
+        return languages;
+    }
+
+    function borders(bor: string[] | undefined): { numericCode: string, name: string }[] {
+        if (bor === undefined) return [];
+        let countries: { numericCode: string, name: string }[] = [];
+        bor.forEach(c => {
+            let coun: { numericCode: string, name: string } | undefined = data.find(d => d.alpha3Code === c);
+            if (coun) {
+                countries.push(coun);
+            }
+        });
+        return countries;
+    }
+
+    let bor: { numericCode: string, name: string }[] = borders(country?.borders);
+
+    return (
+        <main className="c-main">
+            <div className="back">
+                <Link href={"/"}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+                    </svg>
+                    Back
+                </Link>
+            </div>
+            <div className="info">
+                <div className="image">
+                    <Image className="country-page" src={country!.flags.svg} alt={country!.name} width={200} height={150} />
+                </div>
+                <div className="text">
+                    <h2>{country?.name}</h2>
+                    <div className="more-text">
+                        <ul>
+                            <li><p><strong>Native Name:</strong> {country?.nativeName}</p></li>
+                            <li><p><strong>Population:</strong> {country?.population.toLocaleString()}</p></li>
+                            <li><p><strong>Region:</strong> {country?.region}</p></li>
+                            <li><p><strong>Sub Region</strong> {country?.subregion}</p></li>
+                            <li><p><strong>Capital:</strong> {country?.capital}</p></li>
+                        </ul>
+                        <ul>
+                            <li><p><strong>Top Level Domain</strong> {country?.topLevelDomain}</p></li>
+                            <li><p><strong>Currencies</strong> {format(country?.currencies)}</p></li>
+                            <li><p><strong>Languages:</strong> {format(country?.languages)}</p></li>
+                        </ul>
+                    </div>
+                    <div className="border">
+                        <p><strong>Border Countries:</strong></p>
+                        {bor.map(it =>
+                            <Link className="border-country" key={it.numericCode} href={"./" + it.numericCode}>
+                                <span>{it.name}</span>
+                            </Link>)}
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default Country;
---
+++ src/app/page.tsx
@@ -0,0 +1,60 @@
+"use client"
+import Image from "next/image";
+import Link from "next/link";
+import data from "./../../data.json";
+import "./home.css"
+import { useState } from "react";
+
+export default function Home() {
+  const [search, setSearch] = useState("");
+  const [region, setRegion] = useState("");
+
+  const handleSearch = (e: any): void => {
+    setSearch(prev => e.target.value);
+  }
+
+  const handleDrop = (e: any): void => {
+    setRegion(prev => e.target.value);
+  }
+
+  return (
+    <main className="h-main">
+      <div className="filter">
+        <div className="search">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+          </svg>
+          <input onKeyUp={handleSearch} id="search" type="text" placeholder="Search for a country..." />
+        </div>
+
+        <select onChange={handleDrop} name="region" id="region">
+          <option hidden defaultValue={"None"} value="None">Filter by region</option>
+          <option value="All">All</option>
+          <option value="Africa">Africa</option>
+          <option value="Americas">Americas</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </div>
+      <div className="countries">
+        {data.map((country) =>
+          search === "" && (region === "" || region === "All")
+            || search.toLowerCase() === country.name.substring(0, search.length).toLowerCase()
+            && (region === country.region || region === "All" || region === "") ?
+            <Link className="country" key={country.numericCode} href={"./" + country.numericCode}>
+              <div className="image">
+                <Image src={country.flags.svg} alt={country.name} width={200} height={150} />
+              </div>
+              <div className="dets">
+                <p><strong>{country.name}</strong></p>
+                <p><strong>Population:</strong> {country.population.toLocaleString()}</p>
+                <p><strong>Region:</strong> {country.region}</p>
+                <p><strong>Capital:</strong> {country.capital}</p>
+              </div>
+            </Link> : ""
+        )}
+      </div>
+    </main>
+  );
+}
