5670061216180a523d6698f458b1bd9674c9ecae
7savv7 <savchrisostomidhs@hotmail.com>
2025-03-25 21:33:30 +0200

    Configure for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/REST-Countries-API-with-color-theme-switcher-with-Nextjs' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/REST-Countries-API-with-color-theme-switcher-with-Nextjs/' : '',
 };
 
 export default nextConfig;
