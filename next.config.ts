958cfc09e55d0cb0218cc4604f83a2d742642297
7savv7 <savchrisostomidhs@hotmail.com>
2025-03-25 21:48:28 +0200

    commit

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
-  output: 'export',
-  distDir: 'dist',
-  images: {
-    unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/REST-Countries-API-with-color-theme-switcher-with-Nextjs' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/REST-Countries-API-with-color-theme-switcher-with-Nextjs/' : '',
 };
 
 export default nextConfig;
